8c5114d2075fe8974fe5fc547fa2ba392a03b0d5
Kanan Rustamli <kenanrustamli@gmail.com>
2022-09-13 12:10:43 +0400

    new view

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Screen = ({ value }) => {
   return (
-    <div>
+    <>
       <input type="text" placeholder="" value={value} />
-    </div>
+    </>
   );
 };
 
